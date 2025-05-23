fad29ab50413a6c6d66aafa771d61091fee86bba
Steve Sklar <steven@questdb.io>
2025-05-23 09:39:39 -0400

    fluentd 0.0.2

--- fluentd-plugin-grafana/build.sh
+++ fluentd-plugin-grafana/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-podman build -t docker.io/sklarsa/fluentd-loki:0.0.1 .
-podman push docker.io/sklarsa/fluentd-loki:0.0.1
+TAG=0.0.2
+
+podman build --platform linux/amd64 -t docker.io/sklarsa/fluentd-loki:$TAG .
+podman push docker.io/sklarsa/fluentd-loki:$TAG
